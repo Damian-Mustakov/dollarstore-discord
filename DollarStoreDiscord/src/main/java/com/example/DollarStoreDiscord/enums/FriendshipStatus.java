250be062edb05e5036e2017908eb2ac056ad786a
Damian-Mustakov <damianmustakov@gmail.com>
2025-02-01 17:33:53 +0200

    Resetting git repository

--- DollarStoreDiscord/src/main/java/com/example/DollarStoreDiscord/enums/FriendshipStatus.java
+++
@@ -1,7 +0,0 @@
-package com.example.DollarStoreDiscord.enums;
-
-public enum FriendshipStatus {
-    PENDING,
-    ACCEPTED,
-    DECLINED
-}
--- DollarStoreDiscord/src/main/java/com/example/DollarStoreDiscord/enums/RoleType.java
+++
@@ -1,7 +0,0 @@
-package com.example.DollarStoreDiscord.enums;
-
-public enum RoleType {
-    OWNER,
-    ADMIN,
-    GUEST
-}
